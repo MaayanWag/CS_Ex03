358cb17c2e13f0039b4cab6b1efbca18ef84648e
Maayanwag <maayanwag@gmail.com>
2019-05-21 21:17:41 +0300

    initial Garage

---
+++ Ex03.GarageLogic/Garage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public class Garage
+    {
+        private Dictionary<Vehicle, List<VehicleAndOwnerDetails>> m_GarageVehicles;
+        
+        public void AddNewVehicle(Vehicle i_Vehicle, string i_OwnerName, string i_OwnerPhoneNumber)
+        {
+            //TODO: Create VehicleAndOwnerDetails
+            throw new NotImplementedException();
+        }
+
+        public void ShowAllVehicleLicensePlates()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowAllVehicleLicensePlates(params eVehicleState[] i_VehicleStatesTofilter)
+        {
+            // consider PARAMS
+            throw new NotImplementedException();
+        }
+
+        public void InflateTiresToMaximum(string i_VehicleLicensePlate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RefuleVehicle(string i_VehicleLicensePlate, eFuelType i_FuelType, float i_AmountToFuel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChargeVehicle(string i_VehicleLicensePlate, float i_AmountToCharge)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayVehicleUInformation(string i_VehicleLicensePlate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ex03.GarageLogic/VehicleAndOwnerDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    internal class VehicleAndOwnerDetails
+    {
+        private string m_OwnerName;
+        private string m_OwnerPhoneNumber;
+        private eVehicleState m_OwnerVehicleState;
+    }
+}
