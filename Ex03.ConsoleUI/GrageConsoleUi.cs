de291458d83f2a25dde5d336887d57fc0248992f
Yarden <yardenrachamim1993@gmail.com>
2019-05-25 14:13:05 +0300

    Starting the UI

---
+++ Ex03.ConsoleUI/GrageConsoleUi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GarageMain = Ex03.GarageLogic.GarageMain;
+
+namespace Ex03.ConsoleUI
+{
+    public class GarageConsoleUi
+    {
+        private const int k_NumberOfInstructions = 7;
+        private readonly string[] r_InstructionNames = 
+            new string[k_NumberOfInstructions] {};
+
+        public static void StartGarage()
+        {
+            GarageMain.RunGarageSystem();
+            showGarageFunctionality();
+            Console.ReadLine();
+        }
+
+        private static void showGarageFunctionality()
+        {
+            StringBuilder garageFunctionality = new StringBuilder();
+            garageFunctionality.Append($"For insert press 1{Environment.NewLine}");
+            garageFunctionality.Append($"For insert press 1{Environment.NewLine}");
+        }
+    }
+}
--- Ex03.GarageLogic/GarageMain.cs
+++ Ex03.GarageLogic/GarageMain.cs
@@ -3,28 +3,38 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Ex03.GarageLogic
-{ 
+{
     public class GarageMain
     {
         private static Garage m_Garage;
+        private const int k_ElectricCarValidNumberOfWheels = 4;
+        private const float k_ElectricCarValidMaxAirPressure = 31;
+        private const int k_FuelBasedCarValidNumberOfWheels = 4;
+        private const float k_FuelBasedCarValidMaxAirPressure = 31;
+        private const eFuelType k_FuelBasedCarValidFuelType = eFuelType.Octan96;
+        private const float k_FuelBasedCarValidMaxFuelAmount = 55;
+
         //Type[] m_ValidTypes = new Type[5] {typeof(Car), } 
 
         public static void RunGarageSystem()
         {
             m_Garage = new Garage();
-         }
+        }
 
         public static FuelBasedVehicle CreateFuelBasedVehicle(string i_ModelName, string i_LicenseNumber, Wheel[] i_Wheels,
             eFuelType i_FuelType, float i_MaxFuelAmount)
         {
-            FuelBasedVehicle vehicle = new FuelBasedVehicle (i_ModelName, i_LicenseNumber, i_Wheels, i_FuelType, i_MaxFuelAmount)
+            FuelBasedVehicle fuelVehicle = new FuelBasedVehicle(i_ModelName, i_LicenseNumber, i_Wheels, i_FuelType, i_MaxFuelAmount);
 
-            
+            return fuelVehicle;
         }
 
-        public static ElectricVehicle CreateElectricVehicle()
+        public static ElectricVehicle CreateElectricVehicle(string i_ModelName, string i_LicenseNumber, Wheel[] i_Wheels,
+            float i_MaxBatteryTimeAmount)
         {
-            throw new NotImplementedException();
+            ElectricVehicle electricVehicle = new ElectricVehicle(i_ModelName, i_LicenseNumber, i_Wheels, i_MaxBatteryTimeAmount);
+
+            return electricVehicle;
         }
 
         public static Car CreateCar(Vehicle i_VehicleType, eCarColor i_CarColor, eNumberOfDoors i_NumberOfDoors)
@@ -32,13 +42,16 @@ namespace Ex03.GarageLogic
             Car newCar = null;
             ElectricVehicle electricVehicle = i_VehicleType as ElectricVehicle;
             FuelBasedVehicle fuelBasedVehicle = i_VehicleType as FuelBasedVehicle;
-            
-            if(electricVehicle != null)
+
+
+            if (electricVehicle != null)
             {
+                electricCarParamsAreValid(electricVehicle.Wheels, electricVehicle.MaxBatteryTIme);
                 newCar = new Car(electricVehicle, i_CarColor, i_NumberOfDoors);
             }
-            else if(fuelBasedVehicle != null)
+            else if (fuelBasedVehicle != null)
             {
+                fuelBasedCarParamsAreValid(fuelBasedVehicle.Wheels, fuelBasedVehicle.FuelType, fuelBasedVehicle.MaxFuelAmount);
                 newCar = new Car(fuelBasedVehicle, i_CarColor, i_NumberOfDoors);
             }
             else
@@ -49,7 +62,7 @@ namespace Ex03.GarageLogic
 
             return newCar;
         }
-        
+
         public static Motorcycle CreateMotorcycle()
         {
             throw new NotImplementedException();
@@ -59,5 +72,46 @@ namespace Ex03.GarageLogic
         {
             throw new NotImplementedException();
         }
+
+        #region validations
+
+        private static void electricCarParamsAreValid(Wheel[] i_Wheels, float i_MaxBatteryLife)
+        {
+            bool carParamsValid = true;
+
+            foreach (Wheel wheel in i_Wheels)
+            {
+                carParamsValid &= wheel.MaxAirPressure != k_ElectricCarValidMaxAirPressure;
+            }
+
+            carParamsValid &= i_Wheels.Length != k_ElectricCarValidNumberOfWheels;
+            if (!carParamsValid)
+            {
+                //TODO: Create Exception
+                throw new Exception();
+            }
+        }
+
+        private static void fuelBasedCarParamsAreValid(Wheel[] i_Wheels, eFuelType i_FuelType, float i_MaxFuelAmount)
+        {
+            bool carParamsValid = true;
+
+            foreach (Wheel wheel in i_Wheels)
+            {
+                carParamsValid &= wheel.MaxAirPressure != k_FuelBasedCarValidMaxAirPressure;
+            }
+
+            carParamsValid &= i_Wheels.Length != k_FuelBasedCarValidNumberOfWheels;
+            carParamsValid &= i_MaxFuelAmount != k_FuelBasedCarValidMaxFuelAmount;
+
+            if (!carParamsValid)
+            {
+                //TODO: Create Exception
+                throw new Exception();
+            }
+        }
+
+        #endregion
+
     }
 }
